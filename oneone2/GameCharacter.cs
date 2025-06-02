16999594be286fd9527ea5dbced116765e54c8cf
MGusto <felixbjorseth@outlook.com>
2025-06-02 15:01:19 +0200

    Add project files.

---
+++ oneone2/GameCharacter.cs
@@ -0,0 +1,29 @@
+﻿namespace oneone2
+{
+    public class GameCharacter
+    {
+        private string _name;
+        private int _health;
+        private int _strength;
+        private int _stamina;
+        
+
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            _name = name;
+            _health = health;
+            _strength = strength;
+            _stamina = stamina;
+        }
+
+        public void Fight()
+        {
+
+        }
+
+        public void Recharge()
+        {
+
+        }
+    }
+}
---
+++ oneone2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace oneone2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameCharacter boss = new GameCharacter("Boss McEvil", 400, BossDamage(), 10);
+            GameCharacter hero = new GameCharacter("Hero McGood", 100, 20, 40);
+
+        }
+
+        public static int BossDamage()
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, 31);
+        }
+    }
+}
